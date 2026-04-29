215e9704ba850c2397133d42d6f1362df0385893
Yabaiii1107 <jenrick1107hablanavarro@gmail.com>
2026-04-30 05:03:14 +0800

    Refactor Product class and add new products
    I used AI here to give me product names, as well as categories and prices

--- Program.cs
+++ Program.cs
@@ -1,206 +1,23 @@
 using System;
 
-class Product
+namespace ConsoleApp5
 {
-  public int Id;
-  public string Name;
-  public double Price;
-  public int RemainingStock;
-  
-  public void DisplayStock()
-  {
-    Console.WriteLine($"{Id}. {Name} - \n(Stock Left: {RemainingStock})");
-  }
-
-  public void DisplayProduct()
-  {
-    Console.WriteLine($"{Id}. {Name} - \nPrice: ₱{Price} \nStock: {RemainingStock}");
-  }
-
-  public double GetItemTotal(int quantity)
-  {
-    return Price * quantity;
-  }
-
-  public bool HasEnoughStock(int quantity)
-  {
-    return quantity <= RemainingStock;
-  }
-
-  public void DeductStock(int quantity)
-  {
-     RemainingStock -= quantity;  
-  }
-                      
-class CartItem
-{
-    public Product product;
-    public int quantity;
-    public double subtotal;
-}
-
-class Program
-{
-    static void Main()
+    class Program
     {
-        Product[] store = new Product[]
-        {
-            new Product{ Id=1, Name="ASUS ProArt X870E-CREATOR WIFI ATX Motherboard", Price=31100, RemainingStock=5 },
-            new Product{ Id=2, Name="AMD Ryzen™ 3 3200G Processor with Radeon™ Vega 8 Graphics", Price=3800, RemainingStock=10 },
-            new Product{ Id=3, Name="ASUS Dual GeForce RTX™ 5050 OC Edition 8GB GDDR6 128-bit Graphics Card", Price=22400, RemainingStock=8 },
-            new Product{ Id=4, Name="KINGSTON 16GB DDR4 3200MHZ FURY BEAST RAM", Price=9800, RemainingStock=6 },
-            new Product{ Id=5, Name="ANTEC ATOM B650 650W 80+ BRONZE Non-Modular True Rated Power Supply", Price=3087, RemainingStock=2 },
-            new Product{ Id=6, Name="ADATA 1TB LEGEND 710 M.2 PCIe NVMe (ALEG-710-1TCS) Solid State Drive", Price=9500, RemainingStock=4 },
-        };
-        CartItem[] cart = new CartItem[5];
-        int cartCount = 0;
-
-        string choice = "Y";
-
-        do
-        {
-            Console.WriteLine("\n=== STORE MENU ===");
-            for (int i = 0; i < store.Length; i++)
+      static void Main(string[] args)
+        { 
+            Product[] products = new Product[]
             {
-                store[i].DisplayProduct();
-            }
-
-            Console.Write("Enter product number: ");
-            if (!int.TryParse(Console.ReadLine(), out int productNum) || productNum < 1 || productNum > store.Length)
-            {
-                Console.WriteLine("Invalid product number.");
-                continue;
-            }
-
-            Product selected = store[productNum - 1];
-
-            if (selected.RemainingStock == 0)
-            {
-                Console.WriteLine("Product is out of stock.");
-                continue;
-            }
-
-            Console.Write("Enter quantity: ");
-            if (!int.TryParse(Console.ReadLine(), out int qty) || qty <= 0)
-            {
-                Console.WriteLine("Invalid quantity.");
-                continue;
-            }
-
-            if (!selected.HasEnoughStock(qty))
-            {
-                Console.WriteLine("Not enough stock available.");
-                continue;
-            }
-
-            bool found = false;
-            for (int i = 0; i < cartCount; i++)
-            {
-                if (cart[i].product.Id == selected.Id)
-                {
-                    cart[i].quantity += qty;
-                    cart[i].subtotal += selected.GetItemTotal(qty);
-                    found = true;
-                    break;
-                }
-            }
-
-            if (!found)
-            {
-                if (cartCount >= cart.Length)
-                {
-                    Console.WriteLine("CART IS FULL!");
-                  
-                    Console.Write("\nDo you want to see your receipt? (Y/N): ");
-                    string choice1 = Console.ReadLine();
-
-                      if (choice1.ToUpper() == "Y")
-                      {
-                        double grandTotal = 0;
-                        Console.WriteLine("\n=== RECEIPT ===");
-
-                        for (int i = 0; i < cartCount; i++)
-                        {
-                          Console.WriteLine($"x{cart[i].quantity} {cart[i].product.Name} = ₱{cart[i].subtotal}");
-                          grandTotal += cart[i].subtotal;
-                        }
-
-                        Console.WriteLine($"Grand Total: ₱{grandTotal}");
-
-                        double discount = 0;
-                        if (grandTotal >= 5000)
-                        {
-                          discount = grandTotal * 0.10;
-                          Console.WriteLine($"Discount: ₱{discount}");
-                        }
-
-                        double finalTotal = grandTotal - discount;
-                        Console.WriteLine($"Final Total: ₱{finalTotal}");
-
-                        Console.WriteLine("\n=== UPDATED STOCK ===");
-                        for (int i = 0; i < store.Length; i++)
-                        {
-                          store[i].DisplayStock();
-                        }
-
-                        Console.WriteLine("\nProgram End.");
-                        return;
-                    }
-                    else
-                    {
-                      Console.WriteLine("Program End.");
-                      return;
-                    }
-                }
-            }
-
-                cart[cartCount] = new CartItem
-                {
-                    product = selected,
-                    quantity = qty,
-                    subtotal = selected.GetItemTotal(qty)
-                };
-                cartCount++;
-
-            selected.DeductStock(qty);
-            Console.WriteLine("Item added to cart!");
-
-            Console.Write("Add another item? (Y/N): ");
-            choice = Console.ReadLine();
-
-            if (choice.ToUpper()== "N")
-            {
-                double grandTotal = 0;
-                Console.WriteLine("\n=== RECEIPT ===");
-
-                for (int i = 0; i < cartCount; i++)
-                {
-                    Console.WriteLine($"x{cart[i].quantity} {cart[i].product.Name} = ₱{cart[i].subtotal}");
-                    grandTotal += cart[i].subtotal;
-                }
-
-                Console.WriteLine($"Grand Total: ₱{grandTotal}");
-              
-                double discount = 0;
-                if (grandTotal >= 5000)
-                {
-                    discount = grandTotal * 0.10;
-                    Console.WriteLine($"Discount: ₱{discount}");
-                }
-
-                double finalTotal = grandTotal - discount;
-                Console.WriteLine($"Final Total: ₱{finalTotal}");
-
-                Console.WriteLine("\n=== UPDATED STOCK ===");
-                for (int i = 0; i < store.Length; i++)
-                {
-                    store[i].DisplayStock();
-                }
-
-                Console.WriteLine("\nProgram End.");
-            }
-        
-        } while (choice.ToUpper() == "Y");
+                new Product { id=101, name="Instant Noodles Pack", category="Food", price=120, remainingStock=20 },
+                new Product { id=102, name="Chocolate Bar", category="Food", price=50, remainingStock=25 },
+                new Product { id=103, name="Bottled Water 1L", category="Food", price=30, remainingStock=30 },
+                new Product { id=201, name="Plain T-Shirt", category="Clothing", price=300, remainingStock=15 },
+                new Product { id=202, name="Jeans Pants", category="Clothing", price=900, remainingStock=8 },
+                new Product { id=203, name="Hoodie Jacket", category="Clothing", price=1200, remainingStock=6 },
+                new Product { id=301, name="Notebook", category="School Supplies", price=60, remainingStock=18 },
+                new Product { id=302, name="Ballpen (Blue)", category="School Supplies", price=15, remainingStock=50 },
+                new Product { id=303, name="Backpack", category="School Supplies", price=850, remainingStock=9 }
+            };
+      }
     }
 }
-}                          
